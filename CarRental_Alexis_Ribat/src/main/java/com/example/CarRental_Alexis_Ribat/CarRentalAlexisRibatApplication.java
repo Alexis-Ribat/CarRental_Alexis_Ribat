8c09bc8db22266d95627021cb7332bc51c35da22
alrib <alrib@LAPTOP-CBOLI6AJ>
2019-11-16 13:52:12 +0100

    test

---
+++ CarRental_Alexis_Ribat/src/main/java/com/example/CarRental_Alexis_Ribat/CarRentalAlexisRibatApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarRental_Alexis_Ribat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalAlexisRibatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalAlexisRibatApplication.class, args);
+	}
+
+}
---
+++ CarRental_Alexis_Ribat/src/test/java/com/example/CarRental_Alexis_Ribat/CarRentalAlexisRibatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarRental_Alexis_Ribat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalAlexisRibatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
